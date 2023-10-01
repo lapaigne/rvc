5b9da5d29cb233cbb09495dbbb835c866719be7c
Oleg Lialin <lyalin.pa1nt3r@gmail.com>
2023-10-01 21:19:06 +0400

    html5 input fix

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -6,12 +6,11 @@ public class Main : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		var speed = 3;
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(float delta)
 	{
-		
 	}
 }
--- scripts/Player.cs
+++ scripts/Player.cs
@@ -10,7 +10,29 @@ public class Player : KinematicBody2D
 
     public override void _PhysicsProcess(float delta)
     {
-        var input = Input.GetVector("left", "right", "up", "down");
+        //var input = Input.GetVector("left", "right", "up", "down");
+        var input = new Vector2();
+        if (Input.IsActionPressed("left"))
+        {
+            input.x = -1;
+        }
+        if (Input.IsActionPressed("right"))
+        {
+            input.x = 1;
+        }
+        if (Input.IsActionPressed("up"))
+        {
+            input.y = -1;
+        }
+        if (Input.IsActionPressed("down"))
+        {
+            input.y = 1;
+        }
+        input = input.Normalized();
+
+        var label = GetNode<Label>("/root/Main/vel");
+        label.Text = input.ToString();
+
         if (input == Vector2.Zero)
         {
             velocity = (velocity.Length() > (delta * acceleration)) ? velocity - velocity.Normalized() * deceleration * delta : Vector2.Zero;
